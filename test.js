77c2748f9fc59fc75dce97930887182315828629
torumanabe <tooru0530eva@gmail.com>
2022-09-05 19:16:14 +0900

    implementation of axios

--- test.js
+++ test.js
@@ -9,3 +9,17 @@ const testFunc = (a, b, c) => {
     console.log(c, b, a, message);
 };
 testFunc(1, 2, 3);
+
+import axios from "axios";
+this.Request = function () {
+    self._DataListReady = false;
+    let param = self.prm;
+    if (param == null) {
+        param = {};
+    }
+    console.log(JSON.stringify(param));
+
+    axios
+        .post("/rs/mapeditor/php/mapeditor.php", [param])
+        .then((response) => (this.returnData = response));
+};
