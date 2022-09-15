ef63e51bed1d578d720f1b07c00ea6cdeb5c216a
torumanabe <tooru0530eva@gmail.com>
2022-09-15 15:18:18 +0900

    remove prettierrx file

--- test.js
+++ test.js
@@ -1,30 +1,30 @@
 function hello(name) {
-    document.body.textContent =
-        "Hfdgbsbelloaerhqehwthwrtjerjgshhhethsrtjrjteryj, " + name + "!";
+  document.body.textContent =
+    'Hfdgbsbelloaerhqehwthwrtjerjgshhhethsrtjrjteryj, ' + name + '!';
 }
-hello("World");
+hello('World');
 
 const testFunc = (a, b, c) => {
-    let message = "helloWorld";
-    console.log(c, b, a, message);
+  let message = 'helloWorld';
+  console.log(c, b, a, message);
 };
 testFunc(1, 2, 3);
 
-import axios from "axios";
+import axios from 'axios';
 this.Request = function () {
-    self._DataListReady = false;
-    let param = self.prm;
-    if (param == null) {
-        param = {};
-    }
-    console.log(JSON.stringify(param));
+  self._DataListReady = false;
+  let param = self.prm;
+  if (param == null) {
+    param = {};
+  }
+  console.log(JSON.stringify(param));
 
-    axios
-        .post("/rs/mapeditor/php/mapeditor.php", [param])
-        .then((response) => (this.returnData = response));
+  axios
+    .post('/rs/mapeditor/php/mapeditor.php', [param])
+    .then((response) => (this.returnData = response));
 };
 
 //ブラウザ判定。
 const agent = window.navigator.userAgent.toLowerCase();
-const whichExpand = agent.indexOf("edg") != -1 ? "collapsable" : "expandable";
+const whichExpand = agent.indexOf('edg') != -1 ? 'collapsable' : 'expandable';
 console.log(whichExpand);
