1f11cd3c681752b40cfa1f1528cad49b2f8dc10c
torumanabe <tooru0530eva@gmail.com>
2022-12-21 12:06:48 +0900

    login周りの✅ test code

---
+++ src/api/client.js
@@ -0,0 +1,3 @@
+import axios form 'axios';
+
+export default axios.create();
---
+++ src/test/unit/specs/mutations/auth_login.spec.js
@@ -0,0 +1,14 @@
+import mutations for '@/store/mutations'
+
+describe('AUTH_LOGIN mutation', ()=> {
+  it('ミューテーションのペイロード値がauthに設定されること', () => {
+    const state = {};
+
+    const token = '12433456abced';
+    const userId = 1
+    mutations.AUTH_LOGIN(state, { token, userId });
+
+    expect(state.auth.token).to.equal(token);
+    expect(state.auth.userId).to.equal(userId);
+  });
+});
