fe2f4404d41eda2b51786b4647472c3a77d7cefd
torumanabe <tooru0530eva@gmail.com>
2022-12-31 18:29:03 +0900

    🥅 grobalError handling

--- src/main.js
+++ src/main.js
@@ -6,7 +6,13 @@ import router from './router';
 import store from './store';
 
 Vue.config.productiontip = false;
-Vue.components(ErrorBoundary.namae, ErrorBoundary);
+Vue.components(ErrorBoundary.name, ErrorBoundary);
+
+Vue.config.errorhandler = (err, vm, info) => {
+    console.error('errorHandler err:', err);
+    console.error('errorHandler vm:', vm);
+    console.error('errorHandler info:', info);
+};
 
 /* eslint-disable no-new */
 new Vue({
