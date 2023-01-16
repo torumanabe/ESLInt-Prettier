20a37aca33feaec4fde13b7404e84c009f20a4e1
torumanabe <tooru0530eva@gmail.com>
2023-01-16 15:06:44 +0900

    🐛 testが走るように諸々編集

--- src/tests/unit/ index.js
+++ src/tests/unit/ index.js
@@ -8,3 +8,5 @@ testsContext.keys().forEach(testsContext);
 const srcContext = require.context('../../src', true, /^\. \/
 (?!.*(?:main|router)).*(\.js)?$/)
 srcContext.keys().forEach(srcContext);
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.14/vue.min.js"></script>
